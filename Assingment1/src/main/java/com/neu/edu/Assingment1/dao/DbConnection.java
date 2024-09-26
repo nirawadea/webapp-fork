61e8a9da930dfd150f6e35a996ab7d57b8182296
nirawadea <nirawade.a@northeastern.edu>
2024-09-25 22:56:50 -0400

    Update health endpoint method with database check

--- Assingment1/src/main/java/com/neu/edu/Assingment1/dao/DbConnection.java
+++ Assingment1/src/main/java/com/neu/edu/Assingment1/dao/DbConnection.java
@@ -2,26 +2,21 @@ package com.neu.edu.Assingment1.dao;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
-import java.beans.BeanProperty;
+
 @Configuration
-@Component
 public class DbConnection {
 
-     @Value("${spring.datasource.url}")
-     private String host;
-     @Value("${spring.datasource.username}")
-     private String username;
-     @Value(("${spring.datasource.password}"))
-     private String password;
+    @Value("${spring.datasource.url}")
+    private String host;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value(("${spring.datasource.password}"))
+    private String password;
 
-    @Bean
-    public DataSource createConnection()
-    {
+    public DataSource createConnection() {
         return DataSourceBuilder.create().url(host).username(username).password(password).build();
     }
 }
