c2ed297727d88e216e2e1f612408760c8848eada
nirawadea <nirawade.a@northeastern.edu>
2024-10-02 20:55:12 -0400

    Deleting assignment1 folder and updated code in fork for assignment2

---
+++ src/main/java/com/neu/edu/cloudapplication/dao/DbConnection.java
@@ -0,0 +1,22 @@
+package com.neu.edu.cloudapplication.dao;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DbConnection {
+
+    @Value("${spring.datasource.url}")
+    private String host;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value(("${spring.datasource.password}"))
+    private String password;
+
+    public DataSource createConnection() {
+        return DataSourceBuilder.create().url(host).username(username).password(password).build();
+    }
+}
