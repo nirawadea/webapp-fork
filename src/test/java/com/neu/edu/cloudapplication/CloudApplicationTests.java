dfdd73317c5107e21c52f7a6a5f925ce2f03fedd
nirawadea <nirawade.a@northeastern.edu>
2024-10-16 15:48:12 -0400

    Added dependency for H2

--- src/test/java/com/neu/edu/cloudapplication/CloudApplicationTests.java
+++ src/test/java/com/neu/edu/cloudapplication/CloudApplicationTests.java
@@ -2,8 +2,10 @@ package com.neu.edu.cloudapplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CloudApplicationTests {
 
 	@Test
