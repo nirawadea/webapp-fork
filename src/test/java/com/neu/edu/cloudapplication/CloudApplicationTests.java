bc091a3e21d1f6fb8daf751061c04dc6527241fa
nirawadea <nirawade.a@northeastern.edu>
2024-10-30 09:35:41 -0400

    updated CloudApplicationTests file

--- src/test/java/com/neu/edu/cloudapplication/CloudApplicationTests.java
+++ src/test/java/com/neu/edu/cloudapplication/CloudApplicationTests.java
@@ -2,11 +2,22 @@ package com.neu.edu.cloudapplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(properties = {
+		"aws.s3.region=us-east-1",
+		"aws.s3.bucket=test-bucket"
+})
 class CloudApplicationTests {
+
+	// Mock the S3 client so it doesn't require actual AWS connections
+	@MockBean
+	private S3Client s3Client;
 
 	@Test
 	void contextLoads() {
