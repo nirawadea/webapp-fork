1636a7fabc9aa2e551641973ac8960a7cb47db51
nirawadea <nirawade.a@northeastern.edu>
2024-11-28 08:30:52 -0500

    updated DataSourceConfig

--- src/main/java/com/neu/edu/cloudapplication/config/DataSourceConfig.java
+++ src/main/java/com/neu/edu/cloudapplication/config/DataSourceConfig.java
@@ -1,49 +1,61 @@
 package com.neu.edu.cloudapplication.config;
 
-import com.neu.edu.cloudapplication.service.SecretsManagerService;
-import com.zaxxer.hikari.HikariConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.neu.edu.cloudapplication.controllers.HealthController;
 import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueRequest;
+import software.amazon.awssdk.services.secretsmanager.model.GetSecretValueResponse;
 
 import javax.sql.DataSource;
 import java.util.Map;
 
+@Configuration
 public class DataSourceConfig {
 
     @Value("${aws.secret.name}")
     private String secretName;
 
-    private final SecretsManagerService secretsManagerService;
+    @Value("${aws.s3.region}")
+    private String regionName;
 
-    public DataSourceConfig() {
-        this.secretsManagerService = new SecretsManagerService();
-    }
+    private final static Logger logger = LoggerFactory.getLogger(DataSourceConfig.class);
 
     @Bean
     public DataSource dataSource() {
-        Map<String, String> secrets = secretsManagerService.getSecret(secretName);
-
+        Map<String, String> secrets = getSecret(secretName);
         String dbUrl = secrets.get("DB_URL");
         String username = secrets.get("DB_USERNAME");
         String password = secrets.get("DB_PASSWORD");
-
-        // Configure HikariCP DataSource
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(dbUrl);
-        config.setUsername(username);
-        config.setPassword(password);
-        config.setDriverClassName("com.mysql.cj.jdbc.Driver");
-
-        // Optional: Customize HikariCP settings
-        config.setMaximumPoolSize(10);
-        config.setMinimumIdle(5);
-        config.setIdleTimeout(600000);
-        config.setConnectionTimeout(30000);
-        config.setMaxLifetime(1800000);
-
-        return new HikariDataSource(config);
+        return DataSourceBuilder.create()
+                .url(dbUrl)
+                .username(username)
+                .password(password)
+                .type(HikariDataSource.class)
+                .build();
     }
 
+    public Map<String, String> getSecret(String secretName) {
+        SecretsManagerClient secretsManagerClient = SecretsManagerClient.builder().region(Region.of(regionName)).build();
+        GetSecretValueRequest secretValueRequest = GetSecretValueRequest.builder().secretId(secretName).build();
 
+        GetSecretValueResponse secretValueResponse;
+
+        // Parse secret value as JSON
+        try {
+            secretValueResponse = secretsManagerClient.getSecretValue(secretValueRequest);
+            ObjectMapper objectMapper = new ObjectMapper();
+            logger.info("SecretString: " + secretValueResponse.secretString());
+            return objectMapper.readValue(secretValueResponse.secretString(), Map.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to parse secret value", e);
+        }
+    }
 }
